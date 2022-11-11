9f8191bc8a37ac18d729a269a979442e1c782655
Team @MoviZenX <84813706+saikatsingh@users.noreply.github.com>
2022-11-12 01:54:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1015283214"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://link2files:link2files@cluster0.kzvskxk.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://elbdyrql:okSBZCwwfXF3VdZPOO8XGlzk5UgogU6v@peanut.db.elephantsql.com/elbdyrql")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
